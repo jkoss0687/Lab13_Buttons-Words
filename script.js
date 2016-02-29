2de7d5ce71fc3cb9781ac74bd7e088714178b8e1
Jessica <jkoss0687@yahoo.com>
2016-02-29 09:10:02 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// var app = angular.module('talks', []);
+
+// var randomWords = ['JavaScript', 'Class',' Grand Circus', 'Bootcamp', 'Web Developer'];
+
+
+// var words = randomWords[Math.floor(Math.random()*randomWords.length)];
+
+// $scope.ctx.skills = data.result.skills;
+//     $scope.praiseTextArray = [
+//     "Hooray",
+//     "You\'re ready to move to a new skill", 
+//     "Yahoo! You completed a problem", 
+//     "You\'re doing great",  
+//     "You succeeded", 
+//     "That was a brave effort trying new problems", 
+//     "Your brain was working hard",
+//     "All your hard work is paying off",
+//     "Very nice job!, Let\'s see what you can do next",
+//     "Well done",
+//     "That was excellent work",
+//     "Awesome job",
+//     "You must feel good about doing such a great job",
+//     "Right on",
+//     "Great thinking",
+//     "Wonderful work",
+//     "You were right on top of that one",
+//     "Beautiful job",
+//     "Way to go",
+//     "Sensational effort"
+//   ];
+
+// $scope.praiseTextWord = $scope.praiseTextArray[
+// 	Math.floor(Math.random()*$scope.praiseTextArray.length)];
